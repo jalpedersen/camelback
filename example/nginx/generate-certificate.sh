503b59f4bf3122cd1abbdc09fa1b83056aa651f8
Jesper Andre Lyngesen Pedersen <jalpedersen@gmail.com>
2011-02-28 17:24:37 +0300

    modify certificate script a little

--- example/nginx/generate-certificate.sh
+++ example/nginx/generate-certificate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-openssl genrsa -des3 -out server.key 1024 && \
+openssl genrsa -aes256 -out server.key 4096 && \
 
 openssl req -new -key server.key -out server.csr
 
@@ -8,7 +8,8 @@ openssl req -new -key server.key -out server.csr
 cp server.key server.key.org && \
 openssl rsa -in server.key.org -out server.key
 
-#Now sign it
-openssl x509 -req -days 365 -in server.csr \
-             -signkey server.key -out server.crt
-
+if [ x"$1" = x"sign" ]; then
+  #Now sign it
+  openssl x509 -req -days 365 -in server.csr \
+               -signkey server.key -out server.crt
+fi
