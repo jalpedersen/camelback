1b2cf4e37416ef88c5f83360c45f4d2d0f07cbff
Jesper Andre Lyngesen Pedersen <jalpedersen@gmail.com>
2011-02-28 21:47:22 +0300

    Add note about startssl.com

--- example/nginx/generate-certificate.sh
+++ example/nginx/generate-certificate.sh
@@ -13,3 +13,6 @@ if [ x"$1" = x"sign" ]; then
   openssl x509 -req -days 365 -in server.csr \
                -signkey server.key -out server.crt
 fi
+#Is using certifices from startssl.com, you'll also need to do this:
+#curl http://www.startssl.com/certs/sub.class1.server.ca.pem >> server.crt 
+#curl http://www.startssl.com/certs/ca.pem >> server.crt 
