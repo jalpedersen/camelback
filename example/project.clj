880ba60f0c556aabc401b8b3c62118bc2281dd84
Jesper Andre Lyngesen Pedersen <jalpedersen@gmail.com>
2011-05-27 19:14:44 +0400

    Jetty 7.4.2

--- example/project.clj
+++ example/project.clj
@@ -11,6 +11,6 @@
   :dev-dependencies [[swank-clojure "1.2.1"]
 		     [ring/ring-devel "0.3.8"]
   		     [uk.org.alienscience/leiningen-war "0.0.12"]
-                     [org.signaut/ring-jetty7-adapter "0.3.8.1"]]
+                     [org.signaut/ring-jetty7-adapter "0.3.8.2"]]
   :war {:name "example.war"}
   :aot [example.servlet])
