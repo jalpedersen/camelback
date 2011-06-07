9d93740ab2ed9806515b343e231bafe7aca6298c
Jesper Andre Lyngesen Pedersen <jalpedersen@gmail.com>
2011-06-07 23:01:07 +0400

    Sample webapp document

--- example/deploy.sh
+++ example/deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/sh -x
+# To create the initial webapp document
+# curl -XPUT admin@password@localhost:5984/webapps/example -d @webapp.json
 
-java -jar ../../joiner/joiner-0.8-standalone.jar --op update --db messages --id example --file example.war
+java -jar ../../joiner/joiner-0.8-standalone.jar --op update --db webapps --id example --file example.war
