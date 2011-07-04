36f3d162978110c0e35dd27622e810bedb1cb88d
Jesper Andre Lyngesen Pedersen <jalpedersen@gmail.com>
2011-07-04 18:13:55 +0400

    Jetty 7.4.3

--- example/project.clj
+++ example/project.clj
@@ -1,16 +1,16 @@
 (defproject example "1.0.0-SNAPSHOT"
   :description "Example web app using single-sign-on with couchdb"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.slf4j/log4j-over-slf4j "1.6.1"]
                  [ch.qos.logback/logback-classic "0.9.28"]
-		 [compojure "0.6.2"]
+		 [compojure "0.6.4"]
                  [org.signaut/ring.middleware.servlet-ext "0.1"]
-		 [ring/ring-core "0.3.8"]
-		 [ring/ring-servlet "0.3.8"]]
+		 [ring/ring-core "0.3.10"]
+		 [ring/ring-servlet "0.3.10"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
-		     [ring/ring-devel "0.3.8"]
+		     [ring/ring-devel "0.3.10"]
   		     [uk.org.alienscience/leiningen-war "0.0.12"]
-                     [org.signaut/ring-jetty7-adapter "0.3.8.2"]]
+                     [org.signaut/ring-jetty7-adapter "0.3.10"]]
   :war {:name "example.war"}
   :aot [example.servlet])
