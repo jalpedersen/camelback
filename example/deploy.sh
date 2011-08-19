319f61c2adbf28916e33fc2e31697d35ba857824
Jesper Andre Lyngesen Pedersen <jalpedersen@gmail.com>
2011-08-19 16:18:14 +0400

    example: use joiner 0.8.1 in deploy script

--- example/deploy.sh
+++ example/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -x
 # To create the initial webapp document
 # curl -XPUT admin@password@localhost:5984/webapps/example -d @webapp.json
+joiner_jar=../../joiner/joiner-0.8.1-standalone.jar 
 
-java -jar ../../joiner/joiner-0.8-standalone.jar --op update --db webapps --id example --file example.war
+java -jar $joiner_jar --op update --db webapps --id example --file example.war
