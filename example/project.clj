091762bb1aaff664c680234f06b54eed63729e77
Jesper Andre Lyngesen Pedersen <jalpedersen@gmail.com>
2011-10-26 22:26:26 +0400

    jetty 7.5.4

--- example/project.clj
+++ example/project.clj
@@ -7,11 +7,11 @@
                  [ch.qos.logback/logback-classic "0.9.28"]
 		 [compojure "0.6.5"]
                  [org.signaut/ring.middleware.servlet-ext "0.4"]
-		 [ring/ring-core "1.0.0-beta2"]
-		 [ring/ring-servlet "1.0.0-beta2"]]
+		 [ring/ring-core "1.0.0-RC1"]
+		 [ring/ring-servlet "1.0.0-RC1"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
-		     [ring/ring-devel "1.0.0-beta2"]
+		     [ring/ring-devel "1.0.0-RC1"]
   		     [uk.org.alienscience/leiningen-war "0.0.12"]
-                     [org.signaut/ring-jetty7-adapter "1.0.0-beta2"]]
+                     [org.signaut/ring-jetty7-adapter "1.0.0-RC1"]]
   :war {:name "example.war"}
   :aot [example.servlet])
