e0312f80b95aade676c5256b3a2e52d5fb1e3f85
Jesper Andre Lyngesen Pedersen <jalpedersen@gmail.com>
2012-05-29 22:01:28 +0200

    update couchdb build/install script to support ubuntu 12.04

--- setup/install-couchdb.sh
+++ setup/install-couchdb.sh
@@ -1,26 +1,41 @@
 #!/bin/sh
-xulrunner_version=1.9.2.27
-couchdb_version=1.1.1
+#This will compile and install couchdb on an Ubuntu based system
+xulrunner_version=1.9.2.28
+couchdb_version=1.2.0
 prefix=/opt/couchdb
 
+ubuntu_release=`lsb_release -sc`
+
+die() {
+    echo $1
+    exit 1
+}
+cd apache-couchdb-$couchdb_version || die "Cannot find couchdb $couchdb_version"
+
 #dependencies
-sudo apt-get install build-essential erlang xulrunner-dev libicu-dev help2man libcurl4-openssl-dev
+sudo apt-get install build-essential erlang libicu-dev help2man libcurl4-openssl-dev
+if [ "$ubuntu_release" != "precise" ]; then
+    sudo apt-get install build-essential erlang xulrunner-dev libicu-dev help2man libcurl4-openssl-dev
+    ./configure --prefix=$prefix --with-js-lib=/usr/lib/xulrunner-$xulrunner_version/ \
+        --with-js-include=/usr/lib/xulrunner-devel-$xulrunner_version/include || die "Failed to configure couchdb"
+    sudo su -c "echo \"/usr/lib/xulrunner-$xulrunner_version/\" > /etc/ld.so.conf.d/xulrunner.conf" || die "Failed to add xulrunner to ld.so.conf.d"
+
+else
+    sudo apt-get install libmozjs185-dev
+    ./configure --prefix=$prefix || die "Failed to configure couchdb"
+fi
 
 
 (\
-cd apache-couchdb-$couchdb_version && \
-./configure --prefix=$prefix --with-js-lib=/usr/lib/xulrunner-$xulrunner_version/ \
-            --with-js-include=/usr/lib/xulrunner-devel-$xulrunner_version/include &&\
 make && \
 sudo make install \
 ) &&\
-
-sudo su -c "echo \"/usr/lib/xulrunner-$xulrunner_version/\" > /etc/ld.so.conf.d/xulrunner.conf"
 
 
 # Add couchdb user account
 sudo useradd -d $prefix/var/lib/couchdb couchdb 
 sudo chown -R couchdb:couchdb $prefix/var/lib/couchdb $prefix/var/log/couchdb &&\
+sudo chown -R couchdb:couchdb $prefix/var/run/couchdb $prefix/var/log/couchdb &&\
 
 # next two steps fix problems where adding admin hangs or setting admins in local.ini hangs the start. Also fixes problems with reader_acl test.
 sudo chown -R root:couchdb $prefix/etc/couchdb &&\
